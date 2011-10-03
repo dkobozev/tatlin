792ca68d92f0ad4a35d34ce9b9849843963c5064
Denis Kobozev <d.v.kobozev@gmail.com>
2011-10-02 22:30:40 -0400

    Make stl parser more robust

--- libtatlin/stlparser.py
+++ libtatlin/stlparser.py
@@ -1,7 +1,7 @@
 """
 Parser for STL (stereolithography) files.
 """
-from .vector3 import Vector3
+from vector3 import Vector3
 
 
 class StlFile(object):
@@ -45,12 +45,14 @@ endfacet
 
 
 class ParseError(Exception):
+    pass
+
+class InvalidTokenError(ParseError):
     def __init__(self, line_no, msg):
         full_msg = 'parse error on line %d: %s' % (line_no, msg)
-        Exception.__init__(self, full_msg)
+        ParseError.__init__(self, full_msg)
 
-
-class ParseEOF(Exception):
+class ParseEOF(ParseError):
     pass
 
 
@@ -69,7 +71,35 @@ class StlAsciiParser(object):
         self.fname = fname
 
         self.line_no = 0
-        self._line_peek = None
+        self.tokenized_peek_line = None
+
+    def readline(self):
+        line = self.finput.readline()
+        if line == '':
+            raise ParseEOF
+        return line
+
+    def next_line(self):
+        next_line = self.peek_line()
+
+        self.tokenized_peek_line = None # force peak line read on next call
+        self.line_no += 1
+
+        return next_line
+
+    def peek_line(self):
+        if self.tokenized_peek_line is None:
+            while True:
+                line = self.readline()
+                self.tokenized_peek_line = self._tokenize(line)
+                if len(self.tokenized_peek_line) > 0:
+                    break
+
+        return self.tokenized_peek_line
+
+    def _tokenize(self, line):
+        line = line.strip().split()
+        return line
 
     def parse(self):
         """
@@ -78,37 +108,15 @@ class StlAsciiParser(object):
         self.finput = open(self.fname, 'r')
         try:
             self._solid()
-        except ParseEOF:
-            pass
-        self.finput.close()
+        finally:
+            self.finput.close()
 
         return self.facet_list
-
-    def next_line(self):
-        if self._line_peek is None:
-            line = self._tokenize(self.finput.readline())
-        else:
-            line = self._line_peek
-
-        if len(line) == 0:
-            raise ParseEOF
-
-        self._line_peek = self._tokenize(self.finput.readline())
-        self.line_no += 1
-
-        return line
-
-    def peek_line(self):
-        return self._line_peek
-
-    def _tokenize(self, line):
-        line = line.strip().split()
-        return line
 
     def _solid(self):
         line = self.next_line()
         if line[0] != 'solid':
-            raise ParseError(self.line_no, 'expected "%s", got "%s"' % ('solid', line[0]))
+            raise InvalidTokenError(self.line_no, 'expected "%s", got "%s"' % ('solid', line[0]))
 
         self._facets()
         self._endsolid()
@@ -116,7 +124,7 @@ class StlAsciiParser(object):
     def _endsolid(self):
         line = self.next_line()
         if line[0] != 'endsolid':
-            raise ParseError(self.line_no, 'expected "%s", got "%s"' % ('endsolid', line[0]))
+            raise InvalidTokenError(self.line_no, 'expected "%s", got "%s"' % ('endsolid', line[0]))
 
     def _facets(self):
         self.facet_list = []
@@ -128,12 +136,12 @@ class StlAsciiParser(object):
     def _facet(self):
         line = self.next_line()
         if line[0] != 'facet':
-            raise ParseError(self.line_no, 'expected "%s", got "%s"' % ('facet', line[0]))
+            raise InvalidTokenError(self.line_no, 'expected "%s", got "%s"' % ('facet', line[0]))
 
         if line[1] == 'normal':
             self.facet_normal = Vector3(float(line[2]), float(line[3]), float(line[4]))
         else:
-            raise ParseError(self.line_no, 'expected "%s", got "%s"' % ('normal', line[1]))
+            raise InvalidTokenError(self.line_no, 'expected "%s", got "%s"' % ('normal', line[1]))
 
         self._outer_loop()
         self._endfacet()
@@ -141,7 +149,7 @@ class StlAsciiParser(object):
     def _endfacet(self):
         line = self.next_line()
         if line[0] != 'endfacet':
-            raise ParseError(self.line_no, 'expected "%s", got "%s"' % ('endfacet', line[0]))
+            raise InvalidTokenError(self.line_no, 'expected "%s", got "%s"' % ('endfacet', line[0]))
 
         facet = Facet(self.facet_normal, self.vertex_list)
         self.facet_list.append(facet)
@@ -149,7 +157,7 @@ class StlAsciiParser(object):
     def _outer_loop(self):
         line = self.next_line()
         if ' '.join(line) != 'outer loop':
-            raise ParseError(self.line_no, 'expected "%s", got "%s"' % ('outer loop', ' '.join(line)))
+            raise InvalidTokenError(self.line_no, 'expected "%s", got "%s"' % ('outer loop', ' '.join(line)))
 
         self._vertices()
         self._endloop()
@@ -157,7 +165,7 @@ class StlAsciiParser(object):
     def _endloop(self):
         line = self.next_line()
         if line[0] != 'endloop':
-            raise ParseError(self.line_no, 'expected "%s", got "%s"' % ('endloop', line[0]))
+            raise InvalidTokenError(self.line_no, 'expected "%s", got "%s"' % ('endloop', line[0]))
 
     def _vertices(self):
         self.vertex_list = []
@@ -169,7 +177,7 @@ class StlAsciiParser(object):
     def _vertex(self):
         line = self.next_line()
         if line[0] != 'vertex':
-            raise ParseError(self.line_no, 'expected "%s", got "%s"' % ('vertex', line[0]))
+            raise InvalidTokenError(self.line_no, 'expected "%s", got "%s"' % ('vertex', line[0]))
         vertex = Vector3(float(line[1]), float(line[2]), float(line[3]))
         self.vertex_list.append(vertex)
 
