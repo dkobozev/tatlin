c545e3838eb0fdbec58b2321f8da6a313dcf6134
Denis Kobozev <d.v.kobozev@gmail.com>
2011-10-20 00:24:56 -0400

    Add stl panel widgets

--- libtatlin/ui.py
+++ libtatlin/ui.py
@@ -14,49 +14,16 @@ class GcodePanel(gtk.VBox):
         self.app = app
 
         # --------------------------------------------------------------------
-        # DISPLAY
-        # --------------------------------------------------------------------
-
-        label_layers = gtk.Label('Layers')
-        label_layers.set_alignment(-1, 0) # align text to the left
-        self.hscale_layers = gtk.HScale()
-        self.hscale_layers.set_range(1, self.app.get_property('layers_range_max'))
-        self.hscale_layers.set_value(self.app.get_property('layers_value'))
-        self.hscale_layers.set_increments(1, 10)
-        self.hscale_layers.set_digits(0)
-        self.hscale_layers.set_size_request(200, 35)
-
-        self.check_arrows = gtk.CheckButton('Show arrows')
-        self.check_2d = gtk.CheckButton('2D view')
-        self.btn_reset_perspective = gtk.Button('Reset perspective')
-
-        table_display = gtk.Table(rows=2, columns=1)
-        table_display.set_border_width(5)
-        table_display.set_row_spacings(5)
-        table_display.attach(label_layers,               0, 1, 0, 1, yoptions=0)
-        table_display.attach(self.hscale_layers,         0, 1, 1, 2, yoptions=0)
-        table_display.attach(self.check_arrows,          0, 1, 2, 3, yoptions=0)
-        table_display.attach(self.check_2d,              0, 1, 3, 4, yoptions=0)
-        table_display.attach(self.btn_reset_perspective, 0, 1, 4, 5, yoptions=0)
-
-        frame_display = gtk.Frame('Display')
-        frame_display.add(table_display)
-        frame_display.set_border_width(5)
-
-        # --------------------------------------------------------------------
         # DIMENSIONS
         # --------------------------------------------------------------------
 
-        label_width = gtk.Label('Width (x):')
-        label_width.set_alignment(-1, 0)
+        label_width = gtk.Label('X:')
         self.label_width_value = gtk.Label('43 mm')
 
-        label_depth = gtk.Label('Depth (y):')
-        label_depth.set_alignment(-1, 0)
+        label_depth = gtk.Label('Y:')
         self.label_depth_value = gtk.Label('43 mm')
 
-        label_height = gtk.Label('Height (z):')
-        label_height.set_alignment(-1, 0)
+        label_height = gtk.Label('Z:')
         self.label_height_value = gtk.Label('43 mm')
 
         table_dimensions = gtk.Table(rows=3, columns=2)
@@ -73,8 +40,38 @@ class GcodePanel(gtk.VBox):
         frame_dimensions.add(table_dimensions)
         frame_dimensions.set_border_width(5)
 
-        self.pack_start(frame_display, False)
+        # --------------------------------------------------------------------
+        # DISPLAY
+        # --------------------------------------------------------------------
+
+        label_layers = gtk.Label('Layers')
+        label_layers.set_alignment(-1, 0) # align text to the left
+        self.hscale_layers = gtk.HScale()
+        self.hscale_layers.set_range(1, self.app.get_property('layers_range_max'))
+        self.hscale_layers.set_value(self.app.get_property('layers_value'))
+        self.hscale_layers.set_increments(1, 10)
+        self.hscale_layers.set_digits(0)
+        self.hscale_layers.set_size_request(200, 35)
+
+        self.check_arrows = gtk.CheckButton('Show arrows')
+        self.check_2d = gtk.CheckButton('3D view')
+        self.btn_reset_perspective = gtk.Button('Reset perspective')
+
+        table_display = gtk.Table(rows=2, columns=1)
+        table_display.set_border_width(5)
+        table_display.set_row_spacings(5)
+        table_display.attach(label_layers,               0, 1, 0, 1, yoptions=0)
+        table_display.attach(self.hscale_layers,         0, 1, 1, 2, yoptions=0)
+        table_display.attach(self.check_arrows,          0, 1, 2, 3, yoptions=0)
+        table_display.attach(self.check_2d,              0, 1, 3, 4, yoptions=0)
+        table_display.attach(self.btn_reset_perspective, 0, 1, 4, 5, yoptions=0)
+
+        frame_display = gtk.Frame('Display')
+        frame_display.add(table_display)
+        frame_display.set_border_width(5)
+
         self.pack_start(frame_dimensions, False)
+        self.pack_start(frame_display, False)
 
         self.connect_handlers()
 
@@ -90,28 +87,111 @@ class StlPanel(gtk.VBox):
 
         self.app = app
 
-        label_scale = gtk.Label('Factor')
-        self.entry_scale = gtk.Entry()
-        self.entry_scale.set_text('1.0')
-        self.button_scale = gtk.Button('Scale')
+        # --------------------------------------------------------------------
+        # DIMENSIONS
+        # --------------------------------------------------------------------
 
-        table_dimensions = gtk.Table(rows=1, columns=3)
+        label_x = gtk.Label('X:')
+        self.entry_x = gtk.Entry()
+        label_x_units = gtk.Label('mm')
+
+        label_y = gtk.Label('Y:')
+        self.entry_y = gtk.Entry()
+        label_y_units = gtk.Label('mm')
+
+        label_z = gtk.Label('Z:')
+        self.entry_z = gtk.Entry()
+        label_z_units = gtk.Label('mm')
+
+        label_factor = gtk.Label('Factor:')
+        self.entry_factor = gtk.Entry()
+        self.entry_factor.set_text('1.0')
+
+        table_dimensions = gtk.Table(rows=4, columns=3)
         table_dimensions.set_border_width(5)
         table_dimensions.set_row_spacings(5)
-        table_dimensions.attach(label_scale,       0, 1, 0, 1, yoptions=0)
-        table_dimensions.attach(self.entry_scale,  1, 2, 0, 1, yoptions=0)
-        table_dimensions.attach(self.button_scale, 2, 3, 0, 1, yoptions=0)
+
+        table_dimensions.attach(label_x,       0, 1, 0, 1, yoptions=0)
+        table_dimensions.attach(self.entry_x,  1, 2, 0, 1, yoptions=0)
+        table_dimensions.attach(label_x_units, 2, 3, 0, 1, yoptions=0)
+
+        table_dimensions.attach(label_y,       0, 1, 1, 2, yoptions=0)
+        table_dimensions.attach(self.entry_y,  1, 2, 1, 2, yoptions=0)
+        table_dimensions.attach(label_y_units, 2, 3, 1, 2, yoptions=0)
+
+        table_dimensions.attach(label_z,       0, 1, 2, 3, yoptions=0)
+        table_dimensions.attach(self.entry_z,  1, 2, 2, 3, yoptions=0)
+        table_dimensions.attach(label_z_units, 2, 3, 2, 3, yoptions=0)
+
+        table_dimensions.attach(label_factor,      0, 1, 3, 4, yoptions=0)
+        table_dimensions.attach(self.entry_factor, 1, 2, 3, 4, yoptions=0)
 
         frame_dimensions = gtk.Frame('Dimensions')
+        frame_dimensions.set_border_width(5)
         frame_dimensions.add(table_dimensions)
+
+        # --------------------------------------------------------------------
+        # MOVE
+        # --------------------------------------------------------------------
 
         self.button_center = gtk.Button('Center model')
 
-        self.pack_start(frame_dimensions)
-        self.pack_start(self.button_center)
+        frame_move = gtk.Frame('Move')
+        frame_move.set_border_width(5)
+        frame_move.add(self.button_center)
+
+        # --------------------------------------------------------------------
+        # ROTATE
+        # --------------------------------------------------------------------
+
+        self.btn_x_90 = gtk.Button('+90')
+        label_rotate_x = gtk.Label('X:')
+        self.entry_rotate_x = gtk.Entry()
+
+        self.btn_y_90 = gtk.Button('+90')
+        label_rotate_y = gtk.Label('Y:')
+        self.entry_rotate_y = gtk.Entry()
+
+        self.btn_z_90 = gtk.Button('+90')
+        label_rotate_z = gtk.Label('Z:')
+        self.entry_rotate_z = gtk.Entry()
+
+        table_rotate = gtk.Table(rows=3, columns=3)
+        table_rotate.set_border_width(5)
+        table_rotate.set_row_spacings(5)
+
+        table_rotate.attach(self.btn_x_90,       0, 1, 0, 1, yoptions=0)
+        table_rotate.attach(label_rotate_x,      1, 2, 0, 1, yoptions=0)
+        table_rotate.attach(self.entry_rotate_x, 2, 3, 0, 1, yoptions=0)
+
+        table_rotate.attach(self.btn_y_90,       0, 1, 1, 2, yoptions=0)
+        table_rotate.attach(label_rotate_y,      1, 2, 1, 2, yoptions=0)
+        table_rotate.attach(self.entry_rotate_y, 2, 3, 1, 2, yoptions=0)
+
+        table_rotate.attach(self.btn_z_90,       0, 1, 2, 3, yoptions=0)
+        table_rotate.attach(label_rotate_z,      1, 2, 2, 3, yoptions=0)
+        table_rotate.attach(self.entry_rotate_z, 2, 3, 2, 3, yoptions=0)
+
+        frame_rotate = gtk.Frame('Rotate')
+        frame_rotate.set_border_width(5)
+        frame_rotate.add(table_rotate)
+
+        # --------------------------------------------------------------------
+        # DISPLAY
+        # --------------------------------------------------------------------
+
+        self.btn_reset_perspective = gtk.Button('Reset perspective')
+
+        frame_display = gtk.Frame('Display')
+        frame_display.set_border_width(5)
+        frame_display.add(self.btn_reset_perspective)
+
+        self.pack_start(frame_dimensions, False)
+        self.pack_start(frame_move, False)
+        self.pack_start(frame_rotate, False)
+        self.pack_start(frame_display, False)
 
         self.connect_handlers()
 
     def connect_handlers(self):
-        self.button_scale.connect('clicked', self.app.on_button_scale_clicked)
         self.button_center.connect('clicked', self.app.on_button_center_clicked)
