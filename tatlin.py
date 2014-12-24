01784595d05f06813637fa4cd74081540a3b3a02
Denis Kobozev <d.v.kobozev@gmail.com>
2014-12-24 00:53:42 -0500

    Fix modulo by zero error in stl parser

--- tatlin.py
+++ tatlin.py
@@ -36,7 +36,7 @@ def format_float(f):
 
 class App(BaseApp):
 
-    TATLIN_VERSION = '0.2.3'
+    TATLIN_VERSION = '0.2.4'
     TATLIN_LICENSE = """This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
