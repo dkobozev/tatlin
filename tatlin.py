aac617decdef6591161fefa1c13f186fc0f30eac
Denis Kobozev <d.v.kobozev@gmail.com>
2014-12-23 20:52:14 -0500

    Version bump after Mac OS X fixes

--- tatlin.py
+++ tatlin.py
@@ -36,7 +36,7 @@ def format_float(f):
 
 class App(BaseApp):
 
-    TATLIN_VERSION = '0.2.0'
+    TATLIN_VERSION = '0.2.1'
     TATLIN_LICENSE = """This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
