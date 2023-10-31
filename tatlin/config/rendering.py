2378b76bdbde7ce07477cbc365dba2eec7a84cf8
Denis Kobozev <241236+dkobozev@users.noreply.github.com>
2023-10-31 13:56:50 -0400

    Improve platform compatibility (#30)

--- tatlin/config/rendering.py
+++ tatlin/config/rendering.py
@@ -1,36 +1,76 @@
 import os
 import sys
+import logging
 
 def configure_backend():
+    """
+    Configure the GLX/EGL backend for wxWidgets/PyOpenGL cooperation.
+    """
     if sys.platform != 'linux':
         return
 
-    # if we're running under Wayland, the chances are wxGTK was compiled with
-    # --disable-glcanvasegl to support some software that does not yet work
-    # under EGL, like PrusaSlicer (see: https://github.com/prusa3d/PrusaSlicer/issues/9774)
-    # so we need to force wxGTK to use the X11 backend
-    if os.environ.get('XDG_SESSION_TYPE') == 'wayland':
+    conf = {}
+
+    # Check for the existence of a system-wide config file written by a packaging script to help determine the correct
+    # settings for the current environment. The conf resides in /etc/tatlin.conf, but a relative path must be used for this to work in an AppImage.
+    base_dir = os.path.dirname(os.readlink('/proc/self/exe'))
+    possible_paths = [
+        '../../etc/tatlin.conf',
+        '../../../etc/tatlin.conf'
+    ]
+
+    for possible_path in possible_paths:
+        sys_conf_path = os.path.abspath(os.path.join(base_dir, possible_path))
+
+        if os.path.exists(sys_conf_path):
+            logging.info("Loading system-wide config from {}".format(sys_conf_path))
+
+            with open(sys_conf_path) as f:
+                for line in f:
+                    if line.startswith('#'):
+                        continue
+                    key, value = line.strip().split('=')
+                    conf[key] = value
+
+            break
+
+    # but the environment variables take precedence over the system-wide file
+    if os.environ.get('GDK_BACKEND'):
+        conf['GDK_BACKEND'] = os.environ['GDK_BACKEND']
+
+    if os.environ.get('PYOPENGL_PLATFORM'):
+        conf['PYOPENGL_PLATFORM'] = os.environ['PYOPENGL_PLATFORM']
+
+    if conf.get('GDK_BACKEND'):
+        os.environ['GDK_BACKEND'] = conf['GDK_BACKEND']
+    elif os.environ.get('XDG_SESSION_TYPE') == 'wayland':
+        # wxWidgets might be compiled with --disable-glcanvasegl due to Wayland issues like it is on Fedora 38
+        # For example, see
+        #     https://github.com/prusa3d/PrusaSlicer/issues/9774
+        # The workaround is to instruct the underlying GTK to use X11 instead
+        #     https://docs.gtk.org/gtk4/running.html#gdk_backend
         os.environ['GDK_BACKEND'] = 'x11'
 
-    # IMPORTANT: PYOPENGL_PLATFORM must be set before importing PyOpenGL
-    if 'PYOPENGL_PLATFORM' not in os.environ:
-        # use GLX as the default backend as wxWidgets does not support EGL under
-        # X11 on Ubuntu and EGL GLUT support was only added to PyOpenGL in 3.1.7
-        # see: https://github.com/mcfletch/pyopengl/commit/2d2457b4d565bce1c58b76b427e1f9027e8b4bcc
-        os.environ['PYOPENGL_PLATFORM'] = 'x11' # GLX
+    if conf.get('PYOPENGL_PLATFORM'):
+        os.environ['PYOPENGL_PLATFORM'] = conf['PYOPENGL_PLATFORM']
+    else:
+        # PyOpenGL should match the backend wxWidgets are using, except there doesn't seem to be a straightforward way
+        # to determine the backend at runtime
+        os.environ['PYOPENGL_PLATFORM'] = 'x11'
 
-    # monkey-patch PyOpenGL's context retrieval - needed as of 3.1.7
-    from OpenGL import platform, contextdata
+    if conf.get('IGNORE_PLATFORM_CONTEXT'):
+        # Which brings us to the worst case scenario that exists on Arch at the time of writing (Oct 2023): wxWidgets is
+        # compiled with EGL support, but the version of PyOpenGL in the repo is 2.6, which does not yet support EGL,
+        # resulting in PyOpenGL unable to obtain a GLX context. The best we can do under the circumstances it seems is
+        # to ignore the fact a GLX context is not available and hope for the best.
 
-    def getContext(context=None):
-        """getContext() replacement for monkey-patching until this is fixed in PyOpenGL."""
-        if context is None:
-            context = platform.GetCurrentContext()
-            if context is None: # fixed: 0 is a valid context-id
-                from OpenGL import error
-                raise error.Error(
-                    """Attempt to retrieve context when no valid context"""
-                )
-        return context
+        # IMPORTANT: PYOPENGL_PLATFORM must be set before importing OpenGL
+        from OpenGL import platform, contextdata
 
-    contextdata.getContext = getContext
+        def getContext(context=None):
+            if context is None:
+                context = platform.GetCurrentContext()
+
+            return context
+
+        contextdata.getContext = getContext
