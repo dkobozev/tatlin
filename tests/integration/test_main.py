421ac9df518eb9147ea5f1566573c60b22eca667
Denis Kobozev <d.v.kobozev@gmail.com>
2024-03-02 16:33:33 -0500

    Output test coverage

---
+++ tests/integration/test_main.py
@@ -0,0 +1,40 @@
+import unittest
+from tatlin.conf.config import Config
+from tatlin.lib.gl.platform import Platform
+from tatlin.lib.gl.scene import Scene
+from tatlin.lib.model import ModelLoader
+from tatlin.lib.ui.dialogs import ProgressDialog
+from tatlin.lib.ui.window import MainWindow
+
+from tatlin.main import App
+
+
+class MainTest(unittest.TestCase):
+    # def setUp(self):
+    #    self.app = App()
+
+    def test_gcode(self):
+        app = App()
+        window = MainWindow(app)
+        scene = Scene(window)
+        scene.clear()
+
+        config = Config(".asdf")
+        model_loader = ModelLoader("tests/fixtures/gcode/top.gcode")
+        progress_dialog = ProgressDialog()
+        model_loader.load(config, scene, progress_dialog)
+
+    def test_stl(self):
+        app = App()
+        window = MainWindow(app)
+        scene = Scene(window)
+        scene.clear()
+
+        config = Config(".asdf")
+        model_loader = ModelLoader("tests/fixtures/stl/top.stl")
+        progress_dialog = ProgressDialog()
+        model_loader.load(config, scene, progress_dialog)
+
+
+if __name__ == "__main__":
+    unittest.main()
